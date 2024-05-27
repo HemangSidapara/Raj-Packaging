f7c805b7dd3bd18133d994ae4533978838defbb3
Hemang Sidapara <hemang@logisticinfotech.co.in>
2024-05-27 14:44:48 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: Container(
+        color: Colors.white,
+      ),
     );
   }
 }
